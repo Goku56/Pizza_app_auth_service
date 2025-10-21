761c0b1f18adff7da305f3c489566079e4cd4941
Goku <gokulmodi369@gmail.com>
2025-10-21 20:09:41 +0530

    add winston logger

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,12 @@
 import { Config } from './config/index.ts';
 import app from './app.ts';
+import logger from './config/logger.ts';
 
 const startServer = () => {
   const PORT = Config.PORT;
   try {
     app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
+      logger.info('Listening on port', { port: PORT });
     });
   } catch (error) {
     console.error('Error starting the server:', error);

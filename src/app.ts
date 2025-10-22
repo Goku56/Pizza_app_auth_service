ef0394325e9c411dd987b52db3a4e9ac1236f4fb
Goku <gokulmodi369@gmail.com>
2025-10-22 21:35:46 +0530

    global error handler & testing lib setup

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,29 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import { HttpError } from 'http-errors';
+import logger from './config/logger';
 
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('Welcome to the Auth Service');
+});
+
+//global error handler(must be the last middleware)
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
+  logger.error(err.message, { stack: err.stack });
+  const statusCode = err.statusCode || 500;
+
+  res.status(statusCode).json({
+    error: [
+      {
+        type: err.name,
+        message: err.message,
+        path: '',
+        locatioin: '',
+      },
+    ],
+  });
 });
 
 export default app;
